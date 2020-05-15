0d6c9b00146a2f41fc0787cf51526dab6affedc1
wallarug <wallarug@coliemore.com.au>
2020-05-15 22:48:15 +1000

    added sonar

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 import board
 import digitalio
 import adafruit_character_lcd.character_lcd as character_lcd
+from hcsr04 import HCSR04
 from time import sleep
 
 # Using Robo HAT MM1 Servo Pins
@@ -15,10 +16,24 @@ lcd_backlight = digitalio.DigitalInOut(board.D13)
 lcd_columns = 16
 lcd_rows = 2
 
+# Using Robo HAT MM1 RCC Pins
+trig = digitalio.DigitalInOut(board.D7)
+echo = digitalio.DigitalInOut(board.D7)
+
+
+# devices
+sonar = HCSR04(trig, echo)
 lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 
 # count push ups over a particular time.  Reset on restart.
 push_count = 0
-while True:
+
+with HCSR04(trig, echo) as sonar:
+    try:
+        while True:
+            print(sonar.dist_cm())
+            sleep(2)
+    except KeyboardInterrupt:
+        pass
     
