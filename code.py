fb744681b24dfc7277c2ed66b7e09dbf5069ff31
wallarug <wallarug@coliemore.com.au>
2020-05-15 22:42:52 +1000

    initial commit code.py

---
+++ code.py
@@ -0,0 +1,24 @@
+import board
+import digitalio
+import adafruit_character_lcd.character_lcd as character_lcd
+from time import sleep
+
+# Using Robo HAT MM1 Servo Pins
+lcd_rs = digitalio.DigitalInOut(board.D7)
+lcd_en = digitalio.DigitalInOut(board.D8)
+lcd_d7 = digitalio.DigitalInOut(board.D12)
+lcd_d6 = digitalio.DigitalInOut(board.D11)
+lcd_d5 = digitalio.DigitalInOut(board.D10)
+lcd_d4 = digitalio.DigitalInOut(board.D9)
+lcd_backlight = digitalio.DigitalInOut(board.D13)
+
+lcd_columns = 16
+lcd_rows = 2
+
+lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
+
+# count push ups over a particular time.  Reset on restart.
+push_count = 0
+while True:
+    
